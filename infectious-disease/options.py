b7c775786cd054bf098aba969f2b6ac01336adb5
LilaKelland <lila.kelland@gmail.com>
2024-04-05 20:45:25 +0000

    added options initial

---
+++ infectious-disease/options.py
@@ -0,0 +1,73 @@
+sex = ['F', 'M']
+
+
+gender = ['F', 'M', 'O']
+
+pt = [
+    'Prince Edward Island', 
+    'Nova Scotia', 
+    'Québec', 
+    'Ontario', 
+    'Manitoba', 
+    'Saskatchewan', 
+    'Alberta', 
+    'British Columbia', 
+    'Yukon', 
+    'Northwest Territories', 
+    'Iqluit'
+    ]
+
+self_described_health = [
+    'Excellent', 
+    'Very good', 
+    'Good', 
+    'Fair', 
+    'Poor', 
+    'I do not know'
+]
+
+
+health_unit = [
+    "ALGOMA DISTRICT (2226)",
+    "BRANT COUNTY (2227)",
+    "CITY OF HAMILTON (2237)",
+    "CITY OF OTTAWA (2251)",
+    "DURHAM REGION (2230)",
+    "EASTERN ONTARIO (2258)",
+    "GREY BRUCE (2233)",
+    "HALIBURTON, KAWARTHA, PINE RIDGE (2235)",
+    "HALTON REGION (2236)",
+    "HASTINGS & PRINCE EDWARD COUNTIES (2238)",
+    "HURON PERTH PUBLIC HEALTH (5183)",
+    "KINGSTON, FRONTENAC, LENNOX & ADDINGTON (2241)",
+    "LEEDS, GRENVILLE AND LANARK DISTRICT (2243)",
+    "MIDDLESEX-LONDON (2244)",
+    "MOHLTC - PHD (0)",
+    "NIAGARA REGION (2246)",
+    "NORTH BAY PARRY SOUND DISTRICT (2247)",
+    "NORTHWESTERN (2249)",
+    "OXFORD ELGIN-ST.THOMAS (4913)",
+    "PEEL REGION (2253)",
+    "PETERBOROUGH COUNTY-CITY (2255)",
+    "PORCUPINE (2256)",
+    "SIMCOE MUSKOKA DISTRICT (2260)",
+    "SUDBURY AND DISTRICT (2261)",
+    "THUNDER BAY DISTRICT (2262)",
+    "TORONTO (3895)",
+    "WATERLOO REGION (2265)",
+    "WELLINGTON-DUFFERIN-GUELPH (2266)",
+    "WINDSOR-ESSEX COUNTY (2268)",
+    "YORK REGION (2270)",
+    "(blank)",
+]  
+
+vaccine_status = [
+    'unvaccinated',
+    'partially vaccinated'
+    'up to date with latest'
+    '(blank)'
+    'unknown'
+]
+
+
+describe = ['White', 'Black', 'West Asian/Arab', 'South Asian (Indian, ...)', 'East/Southeast Asian (Chinese, ...)', 'Latin American/Hispanic', 'Aboriginal (First Nations, Métis, Inuit,)', 'Another race/Multiple', 'Not stated']
